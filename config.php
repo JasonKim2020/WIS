f0d1f8242429b0164c9e8a85021e19a48777a92d
Jason Kim <hoho4811@gmail.com>
2021-04-14 20:14:56 +1200

    dbcon changed

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
  
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'dbCICD');
+define('DB_SERVER', 'fdb27.biz.nf');
+define('DB_USERNAME', '3661084_market');
+define('DB_PASSWORD', 'b09196633b!');
+define('DB_NAME', '
+3661084_market');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
